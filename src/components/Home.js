fc7f928adf4c6c41918edb169e964b02dbe6b184
sunil <mahtosunilkumar806@gmail.com>
2022-12-19 23:28:02 +0530

    sunil

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home(props) {
     return (
         <div>
            
-            <h1>Home Component</h1>
+            <h1>Home Components</h1>
             <div className="cart-wrapper">
                 <div className="img-wrapper item">
                     <img src="https://www.fdfproject.com/wp-content/uploads/2018/12/iphone-png.png" />
